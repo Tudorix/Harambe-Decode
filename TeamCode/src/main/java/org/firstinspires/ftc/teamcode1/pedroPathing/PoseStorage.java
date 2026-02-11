f8b1e9488a4e4fa699a79e65cbaac686c2cb4c5d
Tudorix <tudorpopescuandrei@gmail.com>
2026-02-11 12:38:38 +0200

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode1/pedroPathing/PoseStorage.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
-
-import com.pedropathing.geometry.Pose;
-public class PoseStorage {
-    public static Pose autoPose = new Pose(64, 0, 0);
-    public static Pose resetPose = new Pose(64,0,0);//public static double startX = 62.5 ,startY = 0, startAngle=180;
-}
